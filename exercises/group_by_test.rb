39f1b337ab83e0180a7fa283e2c16dbac5b17bf6
David Bayendor <dbayendor@gmail.com>
2014-09-21 22:13:49 -0600

    Complete #group_by enumerable

--- exercises/group_by_test.rb
+++ exercises/group_by_test.rb
@@ -1,57 +1,54 @@
 gem 'minitest'
 require 'minitest/autorun'
-require 'minitest/pride'
 
 class GroupByTest < Minitest::Test
-
   def test_group_words_by_length
-    words = ["sue", "alice", "steve", "sally", "adam", "fort", "tops", "dog", "cat"]
-    grouped = words.group_by do |word|
-      word.length
-    end
-    expected = {3=>["sue", "dog", "cat"], 4=>["adam", "fort", "tops"], 5=>["alice", "steve", "sally"]}
+    words = %w(sue alice steve sally adam fort tops dog cat)
+    grouped = words.group_by(&:length)
+    expected = { 3 => %w(sue dog cat), 4 => %w(adam fort tops), 5 => %w(alice steve sally) }
     assert_equal expected, grouped
   end
 
   def test_group_numbers_by_odd_and_even
     numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     odd_and_even = numbers.group_by do |number|
-      # Your code goes here
+      number % 2
     end
-    expected = {1=>[1, 1, 3, 5, 13, 21, 55], 0=>[2, 8, 34]}
+    expected = { 1 => [1, 1, 3, 5, 13, 21, 55], 0 => [2, 8, 34] }
     assert_equal expected, odd_and_even
   end
 
   def test_group_words_by_first_letter
-    skip
-    words = ["ant", "axis", "albatross", "bolt", "badge", "butter", "car", "cdr", "column"]
-    # Your code goes here
-    expected = {"a"=>["ant", "axis", "albatross"], "b"=>["bolt", "badge", "butter"], "c"=>["car", "cdr", "column"]}
+    words = %w(ant axis albatross bolt badge butter car cdr column)
+    words_by_first_letter = words.group_by do |word|
+      word[0]
+    end
+    expected = { 'a' => %w(ant axis albatross), 'b' => %w(bolt badge butter), 'c' => %w(car cdr column) }
     assert_equal expected, words_by_first_letter
   end
 
   def test_group_words_by_uniqueness
-    skip
-    words = ["one", "two", "one", "TWO", "three", "one", "three", "three", "three"]
-    # Your code goes here
-    expected = {"one"=>["one", "one", "one"], "two"=>["two", "TWO"], "three"=>["three", "three", "three", "three"]}
+    words = %w(one two one TWO three one three three three)
+    grouped = words.group_by(&:downcase)
+    expected = { 'one' => %w(one one one), 'two' => %w(two TWO), 'three' => %w(three three three three) }
     assert_equal expected, grouped
   end
 
   def test_group_by_number_of_zeros
-    skip
-    numbers = [1, 3, 500, 200, 4000, 3000, 10000, 90, 20, 500000]
-    # Your code goes here
-    expected = {0=>[1, 3], 2=>[500, 200], 3=>[4000, 3000], 4=>[10000], 1=>[90, 20], 5=>[500000]}
+    numbers = [1, 3, 500, 200, 4000, 3000, 10_000, 90, 20, 500_000]
+    grouped = numbers.group_by do |number|
+      number.to_s.length - 1
+    end
+    expected = { 0 => [1, 3], 2 => [500, 200], 3 => [4000, 3000], 4 => [10_000], 1 => [90, 20], 5 => [500_000] }
     assert_equal expected, grouped
   end
 
   def test_group_by_order_of_magnitude
-    skip
-    numbers = [1, 3, 503, 239, 4938, 3932, 19982, 93, 21, 501787]
-    # Your code goes here
-    expected = {1=>[1, 3], 2=>[93, 21], 3=>[503, 239], 4=>[4938, 3932], 5=>[19982], 6=>[501787]}
+    numbers = [1, 3, 503, 239, 4938, 3932, 19_982, 93, 21, 501_787]
+    grouped = numbers.group_by do |number|
+      number.to_s.length
+    end
+    expected = { 1 => [1, 3], 2 => [93, 21], 3 => [503, 239], 4 => [4938, 3932], 5 => [19_982], 6 => [501_787] }
     assert_equal expected, grouped
   end
-
 end
